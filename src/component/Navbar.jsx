acfec5804c96055183019c2975d5b28f6beaa2ef
ALZEE23 <muhamadalfarizial726@gmail.com>
2025-04-11 02:38:26 +0700

    refactor: mobile friendly

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,18 +3,29 @@ import Logo from "../assets/logo-removebg-preview 1.svg";
 
 export default function Navbar() {
   return (
-        <nav class="flex w-full h-25 px-16 py-10 mt-2">
-            <div class="my-auto justify-between flex w-screen">
-                <img src={Logo} alt="" class="w-16 h-16"/>
-                <div class="space-x-10 my-auto"> 
-                    <Link to="/" class="font-semibold text-lg">Home</Link>
-                    <a href="#about" class="font-semibold text-lg">About</a>
-                    <Link to="/blog" class="font-semibold text-lg">Blog</Link>
-                </div>
-                <div class="my-auto space-x-5">
-                    <Link href="" class="text-lg font-semibold bg-[#4C563C] text-[#FFFFFF] py-3 px-7 rounded-[5px]">Get Started</Link>
-                </div>
-            </div>
-        </nav>
+    <nav className="flex w-full h-25 px-16 py-10 mt-2">
+      <div className="my-auto justify-between flex w-screen">
+        <img src={Logo} alt="" className="w-16 h-16" />
+        <div className="space-x-10 my-auto md:flex hidden">
+          <Link to="/" className="font-semibold text-lg">
+            Home
+          </Link>
+          <a href="#about" className="font-semibold text-lg">
+            About
+          </a>
+          <Link to="/blog" className="font-semibold text-lg">
+            Blog
+          </Link>
+        </div>
+        <div className="my-auto space-x-5">
+          <Link
+            href=""
+            className="text-lg font-semibold bg-[#4C563C] text-[#FFFFFF] py-3 px-7 rounded-[5px]"
+          >
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </nav>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import HeaderImage from "../assets/free-photo-of-idyllic-english-countryside-with-rolling-hills 1.svg"
+import HeaderImage from "../assets/free-photo-of-idyllic-english-countryside-with-rolling-hills 1.svg";
 import Logo from "../assets/logo-removebg-preview 1.svg";
 import AboutImage from "../assets/pexels-photo-17691920 1.svg";
 
@@ -6,33 +6,37 @@ export default function Home() {
   return (
     <div>
       <section>
-        <div class="flex flex-col mt-14">
-          <div class="flex mx-auto h-72">
-            <h1 class="self-center text-8xl text-center font-semibold bg-gradient-to-r from-[#075D07] via-[#5A8A42] to-[#8AC310] bg-clip-text text-transparent m-40">
+        <div className="flex flex-col mt-14">
+          <div className="flex mx-auto h-72">
+            <h1 className="self-center md:text-8xl text-6xl text-center font-semibold bg-gradient-to-r from-[#075D07] via-[#5A8A42] to-[#8AC310] bg-clip-text text-transparent md:m-40 m-10">
               Artikel Pertanian, Solusi untuk Masa Depan
             </h1>
           </div>
-          <div class="relative w-full h-screen flex flex-col">
-            <h1 class="self-center text-2xl">
+          <div className="relative w-full md:h-[90vh] flex flex-col">
+            <h1 className="self-center md:text-2xl md:m-0 mx-10 text-lg text-center">
               Temukan berbagai informasi mengenai pertanian dalam
               artikel-artikel yang tersedia!
             </h1>
-            <img src={HeaderImage} alt="" class="scale-125 absolute bottom-30" />
+            <img
+              src={HeaderImage}
+              alt=""
+              className="scale-125 absolute bottom-40 w-full md:visible invisible"
+            />
           </div>
         </div>
       </section>
 
-      <section id="about">
-        <div class="flex flex-col mt-40 mx-[137px]">
-          <img src={Logo} alt="" class="w-20 h-20" />
+      <section id="about" className="py-20 md:mt-0 mt-40">
+        <div className="flex flex-col md:mx-[137px] m-10">
+          <img src={Logo} alt="" className="w-20 h-20" />
 
-          <div class="flex h-screen w-full ">
-            <div class="mt-14 space-y-20  w-[60%] h-[100%]">
-              <h1 class="text-6xl w-[90%] font-semibold">
+          <div className="flex h-screen w-full">
+            <div className="mt-14 md:space-y-20 md:w-[60%] h-[100%] space-y-10">
+              <h1 className="md:text-6xl text-4xl md:w-[90%] font-semibold">
                 TUMBUHIN Menumbuhkan Pengetahuan Pertanian
               </h1>
-              <div class="flex flex-col gap-16">
-                <h1 class="w-[70%] text-2xl">
+              <div className="flex flex-col md:gap-16 gap-7">
+                <h1 className="md:w-[70%] md:text-2xl text-xl">
                   Tumbuhin adalah platform yang menghadirkan artikel seputar
                   sektor pertanian. Kami menyajikan informasi yang mudah
                   diakses, membantu menemukan jawaban langsung dari artikel yang
@@ -40,18 +44,18 @@ export default function Home() {
                 </h1>
                 <a
                   href=""
-                  class="text-lg font-semibold w-40 text-center text-[#FFFFFF] bg-[#4C563C] p-3 rounded-[5px]"
+                  className="text-lg font-semibold w-40 text-center text-[#FFFFFF] bg-[#4C563C] p-3 rounded-[5px]"
                 >
                   Learn More
                 </a>
               </div>
             </div>
-            <div class="h-full w-[40%] flex pt-10">
-              <img src={AboutImage} alt="" class="w-[390px] h-[585px]" />
+            <div className="h-full w-[40%] flex pt-10">
+              <img src={AboutImage} alt="" className="w-[390px] h-[585px] md:flex hidden" />
             </div>
           </div>
         </div>
-        <div class="mt-20 h-40"></div>
+        <div className="mt-20 h-40"></div>
       </section>
     </div>
   );
