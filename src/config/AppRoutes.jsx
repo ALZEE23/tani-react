3a98a4f3ef4b011edec0ff44678009b59896d876
ALZEE23 <muhamadalfarizial726@gmail.com>
2025-04-12 06:07:26 +0700

    feat: add login page

--- src/config/AppRoutes.jsx
+++ src/config/AppRoutes.jsx
@@ -2,12 +2,15 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Blog from "../pages/Blog";
+import Login from "../pages/Login";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/blog" element={<Blog />} />
+      <Route path="/login" element={<Login />} />
+
     </Routes>
   );
 };
