068b92f2d9832bfeeda0c34d3c046d8c7c753aa3
ALZEE23 <muhamadalfarizial726@gmail.com>
2025-04-13 10:03:39 +0700

    feat: add more pages

--- src/config/AppRoutes.jsx
+++ src/config/AppRoutes.jsx
@@ -3,6 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Blog from "../pages/Blog";
 import Login from "../pages/Login";
+import Register from "../pages/Register";
+import MyBlog from "../pages/MyBlog";
+import BlogDetail from "../pages/BlogDetail";
+import Bookmark from "../pages/Bookmark";
+import FormBlog from "../pages/FormBlog";
+
 
 const AppRoutes = () => {
   return (
@@ -10,7 +16,12 @@ const AppRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/blog" element={<Blog />} />
       <Route path="/login" element={<Login />} />
-
+      <Route path="/register" element={<Register />} />
+      <Route path="/myblog" element={<MyBlog />} />
+      <Route path="/blogdetail/:id" element={<BlogDetail />} />
+      <Route path="/bookmark" element={<Bookmark />} />
+      <Route path="/formblog" element={<FormBlog />} />
+      <Route path="/formblog/:id" element={<FormBlog />} />
     </Routes>
   );
 };
