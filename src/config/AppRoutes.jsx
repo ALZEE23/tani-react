a814eb17fedcfdef75d4565d08de244731cafa21
ALZEE23 <muhamadalfarizial726@gmail.com>
2025-04-14 02:15:03 +0700

    feat: add private route

--- src/config/AppRoutes.jsx
+++ src/config/AppRoutes.jsx
@@ -1,5 +1,6 @@
 // src/AppRoutes.jsx
 import { Routes, Route } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
 import Home from "../pages/Home";
 import Blog from "../pages/Blog";
 import Login from "../pages/Login";
@@ -9,19 +10,23 @@ import BlogDetail from "../pages/BlogDetail";
 import Bookmark from "../pages/Bookmark";
 import FormBlog from "../pages/FormBlog";
 
-
 const AppRoutes = () => {
   return (
     <Routes>
+      
       <Route path="/" element={<Home />} />
       <Route path="/blog" element={<Blog />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/myblog" element={<MyBlog />} />
       <Route path="/blogdetail/:id" element={<BlogDetail />} />
-      <Route path="/bookmark" element={<Bookmark />} />
-      <Route path="/formblog" element={<FormBlog />} />
-      <Route path="/formblog/:id" element={<FormBlog />} />
+
+      
+      <Route element={<PrivateRoute />}>
+        <Route path="/myblog" element={<MyBlog />} />
+        <Route path="/bookmark" element={<Bookmark />} />
+        <Route path="/formblog" element={<FormBlog />} />
+        <Route path="/formblog/:id" element={<FormBlog />} />
+      </Route>
     </Routes>
   );
 };
