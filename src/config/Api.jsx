f0016e31e02d990c99e2a90d0ba9a9e3f4a8743e
ALZEE23 <muhamadalfarizial726@gmail.com>
2025-04-14 02:03:13 +0700

    feat: add more fetch api

--- src/config/Api.jsx
+++ src/config/Api.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api",
   timeout: 5000,
@@ -10,7 +9,6 @@ const API = axios.create({
   },
 });
 
-
 API.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
@@ -19,9 +17,7 @@ API.interceptors.request.use(
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 API.interceptors.response.use(
@@ -34,5 +30,33 @@ API.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+
+
+export const login = (data) => API.post("/login", data);
+export const register = (data) => API.post("/register", data);
+
+
+export const getAllBlogs = () => API.get("/blogs");
+export const getMyBlogs = () => API.get("/myblogs");
+export const createBlog = (formData) =>
+  API.post("/blogs", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+export const updateBlog = (id, formData) =>
+  API.put(`/blogs/${id}`, formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+export const deleteBlog = (id) => API.delete(`/blogs/${id}`);
+
+
+export const getBookmarks = () => API.get("/bookmarks");
+export const addBookmark = (blogId) => API.post("/bookmarks", { blogId });
+export const deleteBookmark = (id) => API.delete(`/bookmarks/${id}`);
+
+
+export const updateUser = (data) => API.put("/user", data);
+
+
+export const getProtectedData = () => API.get("/protected");
 
 export default API;
